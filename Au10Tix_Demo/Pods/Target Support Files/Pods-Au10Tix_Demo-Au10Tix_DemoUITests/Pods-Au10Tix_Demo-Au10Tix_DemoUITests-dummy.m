77f226854fac2f18d8b87f613b04ae75797391b5
Anton Sakovych <Anton.Sakovych@onseo.biz>
2020-11-16 15:14:23 +0200

    Removing PKHUD

--- Au10Tix_Demo/Pods/Target Support Files/Pods-Au10Tix_Demo-Au10Tix_DemoUITests/Pods-Au10Tix_Demo-Au10Tix_DemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Au10Tix_Demo_Au10Tix_DemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Au10Tix_Demo_Au10Tix_DemoUITests
-@end
--- Au10Tix_Demo/Pods/Target Support Files/Pods-Au10Tix_DemoTests/Pods-Au10Tix_DemoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Au10Tix_DemoTests : NSObject
-@end
-@implementation PodsDummy_Pods_Au10Tix_DemoTests
-@end
