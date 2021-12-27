5c9b44eabeed105c0f175615ce860617187dad3d
macserver <macserver@au-hq-macm1.local>
2021-12-27 11:01:18 +0200

    Update Artifacts 2021-December-27_11-00-17 [skip ci]

--- Au10tixSimpleSample/Pods/Target Support Files/Au10tixDetectorManager/Au10tixDetectorManager-xcframeworks.sh
+++ Au10tixSimpleSample/Pods/Target Support Files/Au10tixDetectorManager/Au10tixDetectorManager-xcframeworks.sh
@@ -149,5 +149,5 @@ install_xcframework() {
   echo "Copied $source to $destination"
 }
 
-install_xcframework "${PODS_ROOT}/Au10tixDetectorManager/Au10tixDetectorManager.xcframework" "Au10tixDetectorManager" "framework" "ios-arm64" "ios-arm64_x86_64-simulator"
+install_xcframework "${PODS_ROOT}/Au10tixDetectorManager/Au10tixDetectorManager.xcframework" "Au10tixDetectorManager" "framework" "ios-arm64_x86_64-simulator" "ios-arm64"
 
